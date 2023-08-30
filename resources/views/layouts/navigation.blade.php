5209fb3c7066d03ad73a8cc9542af926395cbce6
ForgottenTrainer <rogerthehedhog.98@gmail.com>
2023-08-30 18:10:00 -0500

    beta v1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Panel de usuario') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dash.index')" :active="request()->routeIs('dash.index')">
+                        {{ __('Panel de control') }}
                     </x-nav-link>
                 </div>
             </div>
